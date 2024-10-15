bef33bdc933225680b24cd6fc4456caca82cc3e6
Markix1 <marco.valdemoro@gmail.com>
2024-10-15 17:50:38 +0200

    Making new changes on the project of the To Do Application

--- 07-To-Do-App/script.js
+++ 07-To-Do-App/script.js
@@ -27,10 +27,43 @@ function showTodoApp (filter) {
     if (tasks) {
         tasks.forEach((task, id) => {
             let completedTask = task.status === 'completed' ? 'checked' : ''
+
+            if (filter === task.status || filter === null) {
+                liID += `<li class="task">
+                    <label for="${id}">
+                        <input type="checkbox" onClick="updateStatus(this)" id="${id}"${completedTask} />
+                        <p class="${completedTask}">${task.name}</p>
+                    </label>
+                    <div class="settings">
+                        <i class="uil uil-ellipsis" onClick="showMenu(this)"></i>
+                        <ul class="task-menu">
+                            <li onclick="editTask(${id}, "${task.name}")"><i class="uil uil-pen"></i>Edit</li>
+                            <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                        </ul>
+                    </div>
+                </li>`
+            }
         });
     }
+
+    // Si no existe un id, mostrar mensaje de lista de tareas vacía ya que significa que no
+    // hay tareas creadas
+    $taskBox.innerHTML = liID || "<span>You don't have any tasks</span>"
+    let checkTask = document.querySelectorAll('.task')
+    !checkTask.length ? clearAll.classList.remove('active') : clearAll.classList.add('active')
+    $taskBox.offsetHeight >= 300 ? $taskBox.classList.add('overflow') : $taskBox.classList.remove('overflow')
 }
-function showMenu () { }
+
+// Mostrar todos los elementos HTML creados anteriormente
+showTodoApp('all')
+
+function showMenu (selectedTask) { 
+    let containerMenu = selectedTask.parentElement.lastElementChild
+    containerMenu.classList.add('show')
+    document.addEventListener('click', (event) => {
+        if (event.target.value !== 'I' || event.target !== selectedTask) containerMenu.classList.remove('show')
+    })
+}
 function updateStatus () { }
 function editTask () { }
 function deleteTask () { }
